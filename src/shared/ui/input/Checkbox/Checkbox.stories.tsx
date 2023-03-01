a074cfa61f3d8eab854459970cc8569bef2fada4
OddEer0 <OddEer0@gmail.com>
2023-03-01 13:56:06 +0600

    [feature/864e1jpg6]: add checkbox ui component

--- src/shared/ui/input/Checkbox/Checkbox.stories.tsx
+++ src/shared/ui/input/Checkbox/Checkbox.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { BiAngry, BiHappy } from "react-icons/bi"
 
 import { Checkbox } from "./Checkbox"
 
@@ -10,3 +11,50 @@ export default {
 const Template: ComponentStory<typeof Checkbox> = args => <Checkbox {...args} />
 
 export const Default = Template.bind({})
+
+export const Size = () => (
+	<>
+		<h2>small</h2>
+		<Checkbox checkboxSize="small" />
+		<h2>default medium</h2>
+		<Checkbox />
+		<h2>large</h2>
+		<Checkbox checkboxSize="large" />
+	</>
+)
+
+export const LabelPlacement = () => (
+	<>
+		<Checkbox label="right" />
+		<Checkbox label="top" placement="top" />
+		<Checkbox label="left" placement="left" />
+		<Checkbox label="bottom" placement="bottom" />
+	</>
+)
+
+export const Color = () => (
+	<>
+		<h2>default primary</h2>
+		<Checkbox label="text" />
+		<h2>secondary</h2>
+		<Checkbox label="text" color="secondary" />
+		<h2>tertiary</h2>
+		<Checkbox label="text" color="tertiary" />
+		<h2>quaternary</h2>
+		<Checkbox label="text" color="quaternary" />
+		<h2>success</h2>
+		<Checkbox label="text" color="success" />
+		<h2>danger</h2>
+		<Checkbox label="text" color="danger" />
+		<h2>warning</h2>
+		<Checkbox label="text" color="warning" />
+	</>
+)
+
+export const Icon = () => (
+	<>
+		<Checkbox checkboxSize="small" checkedIcon={<BiHappy />} uncheckedIcon={<BiAngry />} />
+		<Checkbox checkedIcon={<BiHappy />} uncheckedIcon={<BiAngry />} />
+		<Checkbox checkboxSize="large" checkedIcon={<BiHappy />} uncheckedIcon={<BiAngry />} />
+	</>
+)
--- src/shared/ui/input/Checkbox/Checkbox.types.ts
+++ src/shared/ui/input/Checkbox/Checkbox.types.ts
@@ -1,3 +1,15 @@
-export interface CheckboxProps {
+import { InputHTMLAttributes, ReactNode } from "react"
+
+type SizeType = "small" | "medium" | "large"
+type ColorType = "primary" | "secondary" | "tertiary" | "quaternary" | "success" | "danger" | "warning"
+type PlacementType = "top" | "bottom" | "left" | "right"
+
+export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+	checkboxSize?: SizeType
+	color?: ColorType
+	label?: Nullable<string>
+	placement?: PlacementType
+	uncheckedIcon?: Nullable<ReactNode>
+	checkedIcon?: Nullable<ReactNode>
 	className?: string
 }
