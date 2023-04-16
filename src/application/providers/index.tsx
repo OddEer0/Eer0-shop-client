58d236f89e2df204f5397b66d7ddc437689099cb
OddEer0 <OddEer0@gmail.com>
2023-04-16 20:13:43 +0600

    [feature/864eeb8jt]: add lazy motion

--- src/application/providers/index.tsx
+++ src/application/providers/index.tsx
@@ -2,8 +2,9 @@ import { FC, PropsWithChildren } from "react"
 
 import { Compose } from "@/shared/ui"
 
+import { WithLazyMotionProvider } from "./WithLazyMotion"
 import { WithThemeProvider } from "./WithThemeProvider"
 
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
-	return <Compose providers={[WithThemeProvider]}>{children}</Compose>
+	return <Compose providers={[WithThemeProvider, WithLazyMotionProvider]}>{children}</Compose>
 }
