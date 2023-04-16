7b02bcd994d19897907675c2cf9fdf0cc1cfdcfd
OddEer0 <OddEer0@gmail.com>
2023-04-16 20:20:07 +0600

    [feature/864eeb8jt]: remove lazy motion

--- src/application/providers/WithLazyMotion.tsx
+++
@@ -1,8 +0,0 @@
-import { LazyMotion } from "framer-motion"
-import { FC, PropsWithChildren } from "react"
-
-const loadFeatures = () => import("./features").then(res => res.default)
-
-export const WithLazyMotionProvider: FC<PropsWithChildren> = ({ children }) => {
-	return <LazyMotion features={loadFeatures}>{children}</LazyMotion>
-}
