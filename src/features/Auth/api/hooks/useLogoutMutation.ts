ab8f00726c312e7f9ad034d71b706659b0986b87
OddEer0 <OddEer0@gmail.com>
2023-04-23 09:43:39 +0600

    [refactor/864efrexe]: fix logout mutation

--- src/features/Auth/api/hooks/useLogoutMutation.ts
+++ src/features/Auth/api/hooks/useLogoutMutation.ts
@@ -1,8 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { authService } from "@/shared/api"
+import { useSsr } from "@/shared/hooks"
 
 export const useLogoutMutate = (callback?: () => void) => {
+	const { isBrowser } = useSsr()
 	const queryClient = useQueryClient()
 
 	return useMutation({
@@ -11,8 +13,11 @@ export const useLogoutMutate = (callback?: () => void) => {
 		onSuccess() {
 			if (callback) {
 				callback()
-				queryClient.setQueryData(["profile"], null)
-				queryClient.setQueryData(["cart"], null)
+			}
+			queryClient.setQueryData(["profile"], null)
+			queryClient.setQueryData(["cart"], null)
+			if (isBrowser) {
+				localStorage.removeItem("isAuth")
 			}
 		}
 	})
